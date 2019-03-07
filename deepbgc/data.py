91020a4267988bcd58cea725ca29ed84ef1c3aaa
David Příhoda <david.prihoda1@merck.com>
2019-03-07 16:39:34 +0100

    Add 0.1.0 model download links.

--- deepbgc/data.py
+++ deepbgc/data.py
@@ -18,6 +18,47 @@ PFAM_CLANS_FILE_NAME = 'Pfam-A.{}.clans.tsv'.format(PFAM_DB_VERSION)
 
 DOWNLOADS = [
     {
+        'url': 'https://github.com/Merck/deepbgc/releases/download/v{}/deepbgc.pkl'.format(DATA_RELEASE_VERSION),
+        'target': 'deepbgc.pkl',
+        'checksum': '7e9218be79ba45bc9adb23bed3845dc1',
+        'versioned': True
+    },
+    {
+        'url': 'https://github.com/Merck/deepbgc/releases/download/v{}/clusterfinder_original.pkl'.format(DATA_RELEASE_VERSION),
+        'target': 'clusterfinder_original.pkl',
+        'dir': 'detector',
+        'checksum': '2ca2429bb9bc99a401d1093c376b37aa',
+        'versioned': True
+    },
+    {
+        'url': 'https://github.com/Merck/deepbgc/releases/download/v{}/clusterfinder_retrained.pkl'.format(DATA_RELEASE_VERSION),
+        'target': 'clusterfinder_retrained.pkl',
+        'dir': 'detector',
+        'checksum': '65679a3b61c562ff4b84bdb574bb6d93',
+        'versioned': True
+    },
+    {
+        'url': 'https://github.com/Merck/deepbgc/releases/download/v{}/clusterfinder_geneborder.pkl'.format(DATA_RELEASE_VERSION),
+        'target': 'clusterfinder_geneborder.pkl',
+        'dir': 'detector',
+        'checksum': 'ca4be7031ae9f70780f17c616a4fa5b5',
+        'versioned': True
+    },
+    {
+        'url': 'https://github.com/Merck/deepbgc/releases/download/v{}/product_activity.pkl'.format(DATA_RELEASE_VERSION),
+        'target': 'product_activity.pkl',
+        'dir': 'classifier',
+        'checksum': 'ca4be7031ae9f70780f17c616a4fa5b5',
+        'versioned': True
+    },
+    {
+        'url': 'https://github.com/Merck/deepbgc/releases/download/v{}/product_class.pkl'.format(DATA_RELEASE_VERSION),
+        'target': 'product_class.pkl',
+        'dir': 'classifier',
+        'checksum': 'ca4be7031ae9f70780f17c616a4fa5b5',
+        'versioned': True
+    },
+    {
         'url': 'ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam{}/Pfam-A.hmm.gz'.format(PFAM_DB_VERSION),
         'target': PFAM_DB_FILE_NAME,
         'gzip': True,
